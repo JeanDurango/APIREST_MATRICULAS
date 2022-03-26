11695fa56336afd735840e2e5409a74960439732
JeanDurango <joyzeua@gmail.com>
2022-03-26 09:56:57 -0500

    apirest

--- Modelo/ListarTodosLosDepartamentos.php
+++ Modelo/ListarTodosLosDepartamentos.php
@@ -3,8 +3,7 @@
     header('Access-Control-Credential: true');
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
-    header('Acess-Control-Allow-Headers: Origin, Content-Type,
-    X-Auth-Token, Authorization');
+    header('Acess-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 
     // vamos a incluir a la base de datos 
     include '../Conexion/ParametrosDB.php';
@@ -13,16 +12,16 @@
     $conn = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
     // vamos avalidar la conexion 
-    if ($Conn->connect_error)
+    if ($conn->connect_error)
         {
-          die("La conexion no se pudo realizar...".$Conn->connect_error);
+          die("La conexion no se pudo realizar...".$conn->connect_error);
         }
         else
         {
             // ahora vamos a construir la consulta para visualizatr todos los registro 
             $sql = "Select * from Tbldepartamento";
             // ahora la ejecutamos
-            $result = $Conn->query($sql);
+            $result = $conn->query($sql);
             // vamos a verificar si tiene registros o no 
             if($result->num_rows >0)
             {
@@ -37,10 +36,10 @@
             }
             else
             {
-                echo "La consulta no tiene registro"
+                echo "La consulta no tiene registro";
             }
             echo $json; //Muestro los datos en formato Json
             // cerrar la conexion a la base de datos 
-            $Conn->close();
+            $conn->close();
         }
 ?>
